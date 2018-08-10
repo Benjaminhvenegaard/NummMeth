00633433a24c7f4cd1d20326db4fa9b441909171
Benjamin Rømer Hvenegaard <behve15@student.sdu.dk>
2018-08-10 21:17:33 +0200

    Done with lecture 4 second time

--- NummMeth-problems/Lecture4-task/Source4.cpp
+++ NummMeth-problems/Lecture4-task/Source4.cpp
@@ -1,18 +1,24 @@
+
+//Includes:
 #include <iostream>
 #include <fstream>
 #include <math.h>
 
+
+//H-files Numerical recipies:
 #include "../Source Code/code/nr3.h"
 #include "../Source Code/code/svd.h"
 #include "../Source Code/code/utilities.h"
+
 
 using namespace std;
 using namespace util;
 
 
-int main() {
+int main()
+{
 	VecDoub xFilip(82); VecDoub yFilip(82);
-	ifstream Filip("src/FilipData.dat"); // be aware of the path.
+	ifstream Filip("../Lecture2-task/FilipData.dat");
 	for (int i = 0; i < 82; i++) {
 		Filip >> yFilip[i];
 		Filip >> xFilip[i];
@@ -25,25 +31,44 @@ int main() {
 			A[i][j] = pow(xFilip[i], (double)j);
 	}
 
-	//see NUMERICAL RECIPIES p. 69-70
-	SVD SVD_A(A);
+
+	SVD SVD_A = A;
+	
 	VecDoub x(A.ncols());
 	VecDoub x2(A.ncols());
-
-	//Writing the solver yourself
+	
+	
 	MatDoub U = SVD_A.u;
 	MatDoub V = SVD_A.v;
 	VecDoub W = SVD_A.w;
 
 	MatDoub Winv = MatDoub(W.size(), W.size());
-	for (int i = 0; i< W.size(); i++)
-		Winv[i][i] = 1.0 / W[i];
 
-	x = V * Winv*T(U)*yFilip;
-	//Alternatively using the NR3 solver with threshold value of eps
+	for (size_t i = 0; i < W.size(); i++)
+	{
+		for (size_t j = 0; j < W.size(); j++)
+		{
+			Winv[i][j] = 0;
+		}
+	}
+	for (size_t i = 0; i < W.size(); i++)
+	{
+		Winv[i][i] = 1.0 / W[i];
+	}
+
+	
+
+	x = V * Winv * (Transpose(U) * yFilip);
+
 	SVD_A.solve(yFilip, x2, SVD_A.eps);
 	cout << "Solution Filip using SVD:" << endl;
-	x.print();
-	x2.print();
+	print(x,"x with my own SVD");
+	print(x2,"x with build in SVD");
+	cout << endl;
+	print(Winv, "Winv");
+
+
+
+	cin.ignore();
 	return 0;
 }
